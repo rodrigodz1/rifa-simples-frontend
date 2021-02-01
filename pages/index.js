ed82d73e95bbc574ed62fe3fd21d21c77348228b
Rodrigo Brito <spensmusic@gmail.com>
2021-02-01 14:11:31 -0300

    header

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,25 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="px-8 m-5 text-xl font-medium">
+    <div className="">
       <Head>
         <title>Dream Draw - Faça sua aposta</title>
         <link rel="icon" href="/clover.svg" />
       </Head>
-      Dream Cash - Multiplique suas chances de ficar rico
-      
+      <div className="flex border border-black p-5 justify-around bg-black">
+        <div className="text-white">
+          <a className="rounded-md hover:bg-gray-700 p-1" href="/">Início</a>
+        </div>
+
+        <div className="text-white">
+          <a className="rounded-md hover:bg-gray-700 p-1" href="/">Sorteios</a>
+        </div>
+
+        <div className="text-white">
+          <a className="rounded-md hover:bg-gray-700 p-1" href="/">Instagram</a>
+        </div>
+      </div>
     </div>
+
   )
 }
