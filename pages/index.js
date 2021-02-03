1572ca97dce6766277a7d99683c9527736777ff5
Rodrigo Brito <spensmusic@gmail.com>
2021-02-03 10:09:07 -0300

    try6

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,19 @@ export default function Home() {
 
       <body className="flex flex-col">
 
-        <Header />
-        <Content />
-        <Footer />
+        <div className="flex justify-between px-20 bg-black shadow-xl">
+          <img className="max-h-16 max-w-16 justify-self-center" src="images/fingers.svg" href="/" />
+          <a className="transition duration-300 text-white text-xl py-4 tracking-wide font-bold uppercase hover:text-yellow-400 border-b-4 border-black hover:border-yellow-400" href="/">Sorteios</a>
+          <a className="transition duration-300 text-white text-xl py-4 tracking-wide font-bold uppercase border-b-4 border-black hover:border-gray-400" href="/">Instagram</a>
+        </div>
+
+        <div className="h-screen bg-gray-200">
+          Conteúdo
+		    </div>
+
+        <div className="w-full text-center p-8 bg-black">
+          <div className="text-white">well thats the footer... for now :)</div>
+        </div>
 
       </body>
 
