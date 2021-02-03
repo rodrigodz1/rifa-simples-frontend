87179ee0d8264cdb8fa479e460b1afedc1bd9faf
Rodrigo Brito <spensmusic@gmail.com>
2021-02-02 23:44:02 -0300

    improve header

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,17 @@ export default function Home() {
         <title>Dream Draw - Faça sua aposta</title>
         <link rel="icon" href="/clover.svg" />
       </Head>
-      <div className="grid grid-cols-3 border border-black px-4 bg-black">
+      <div className="flex justify-between border border-black px-20 bg-black shadow-xl">
         <div className="text-white text-xl rounded-md justify-self-center">
-          <img className="max-h-16 pr-6" src="images/fingers.svg" href="/" />
+          <img className=" max-h-16 pr-6" src="images/fingers.svg" href="/" />
         </div>
 
-        <div className="text-white text-xl rounded-md pt-5 justify-self-center">
-          <a className="rounded-md hover:text-red-500 px-2" href="/">Sorteios</a>
-        </div>
+        <a className="text-white text-xl py-4 tracking-wide font-bold uppercase hover:text-yellow-400 border-b-4 border-black hover:border-yellow-400" href="/">Sorteios</a>
+        <a className="text-white text-xl py-4 tracking-wide font-bold uppercase border-b-4 border-black hover:border-gray-400" href="/">Instagram</a>
+      </div>
 
-        <div className="text-white text-xl rounded-md pt-5 justify-self-center">
-          <a className="rounded-md hover:text-red-500 px-2" href="/">Instagram</a>
-        </div>
+      <div>
+
       </div>
     </div>
 
