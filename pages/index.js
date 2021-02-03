3c4a32e4f19a35f830ae6055800fc468daac6be2
Rodrigo Brito <spensmusic@gmail.com>
2021-02-02 23:52:25 -0300

    transition effect header links

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
           <img className=" max-h-16 pr-6" src="images/fingers.svg" href="/" />
         </div>
 
-        <a className="text-white text-xl py-4 tracking-wide font-bold uppercase hover:text-yellow-400 border-b-4 border-black hover:border-yellow-400" href="/">Sorteios</a>
-        <a className="text-white text-xl py-4 tracking-wide font-bold uppercase border-b-4 border-black hover:border-gray-400" href="/">Instagram</a>
+        <a className="transition duration-300 text-white text-xl py-4 tracking-wide font-bold uppercase hover:text-yellow-400 border-b-4 border-black hover:border-yellow-400" href="/">Sorteios</a>
+        <a className="transition duration-300 text-white text-xl py-4 tracking-wide font-bold uppercase border-b-4 border-black hover:border-gray-400" href="/">Instagram</a>
       </div>
 
       <div>
