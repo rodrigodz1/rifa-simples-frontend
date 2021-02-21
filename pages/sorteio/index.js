f476b997a2b423c2f2ede51032a5a0e99bb7b115
Rodrigo Brito <rodrigo@pop-os.localdomain>
2021-02-21 18:35:22 -0300

    responsive cell

--- pages/sorteio/index.js
+++ pages/sorteio/index.js
@@ -72,7 +72,7 @@ function Content({ raffles }) {
 
 
 
-                <div className="grid grid-cols-12 border border-black mx-10">
+                <div className="grid md:grid-cols-5 grid-cols-10 border border-black mx-10">
                     <Btn num={1000} />
                 </div>
 
