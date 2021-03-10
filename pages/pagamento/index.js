5699df8ed8d676d4aafcdfc787cd81066bd97048
Rodrigo Brito <rodrigo@pop-os.localdomain>
2021-03-10 13:43:19 -0300

    refact payment page

--- pages/pagamento/index.js
+++ pages/pagamento/index.js
@@ -37,11 +37,15 @@ function Content({ router: { query } }) {
                         setConta(props.account[0])
                         setTitular(props.owner)
                         setBanco(props.name)
+
                     }
                     else {
                         setIsBankInfoVisible(false)
                     }
-                }} className={"bg-" + props.color + "-500 p-2 rounded-md shadow-xl text-white"}>{props.name}</button>
+                }} className={"shadow-xl p-2 rounded-md mx-1 text-black font-bold hover:bg-yellow-300"}>
+
+                    <img className={props.padding + ""} src={props.img} />
+                </button>
             </div>
         )
     }
@@ -56,22 +60,32 @@ function Content({ router: { query } }) {
 
                     <div className="text-center pt-4">
                         <p>Oi, {name}</p>
-                        escolha um método de pagamento
-                        em seguida nos envie o comprovante.
+                        <p className="">Escolha um método de pagamento.</p>
+                        <p>Em seguida nos envie o comprovante.</p>
                         <div className="text-center bg-green-200 font-bold">
                             Pedido número: #{id}
                         </div>
-                        <div className="grid grid-cols-3 justify-between bg-yellow-100 py-12">
-                            <Bank name="Nubank" owner="RODRIGO DE BRITO" account={[1234, 4321]} color="purple" />
-                            <Bank name="Inter" owner="RODRIGO DE BRITO" account={[5678, 8765]} color="red" />
-                            <Bank name="Banco do Brasil" owner="RODRIGO DE BRITO" account={[91011, 11109]} color="yellow" />
+                        <div className="grid grid-cols-4 justify-between bg-yellow-100 py-12">
+                            <Bank name="Nubank" img="/nb.png" owner="RODRIGO DE BRITO" account={[1234, 4321]} />
+                            <Bank name="Banco do Brasil" padding="pt-3" img="/bb.png" owner="RODRIGO DE BRITO" account={[91011, 11109]} />
+                            <Bank name="PIX" img="/pix.png" owner="RODRIGO DE BRITO" account={[91011, 11109]} />
+                            <Bank name="PicPay" padding="pt-3" img="/pp.png" owner="RODRIGO DE BRITO" account={[91011, 11109]} />
 
                         </div>
                         {isBankInfoVisible ? <div className="bg-yellow-200 pl-24 text-left">
-                            <p>Banco: {banco}</p>
-                            <p>Titular: {titular}</p>
-                            <p>Agência: {agencia} </p>
-                            <p>Conta: {conta} </p>
+                            {
+                                //bancos com titular, ag, conta, etc...
+                                banco === 'Nubank' || banco === 'Banco do Brasil' ? <div><p>Banco: {banco}</p>
+                                    <p>Titular: {titular}</p>
+                                    <p>Agência: {agencia} </p>
+                                    <p>Conta: {conta} </p></div> : null
+                            }
+                            {
+                                banco === 'PIX' ? <div>a definir pix</div> : null
+                            }
+                            {
+                                banco === 'PicPay' ? <div>a definir picpay</div> : null
+                            }
                         </div> : null}
                     </div>
 
@@ -90,7 +104,7 @@ function Content({ router: { query } }) {
 
                 </div>
                 <div className="text-center h-full">
-                    <button className="bg-green-600 text-gray-100 p-2 mt-20 rounded-md shadow-xl">Enviar comprovante</button>
+                    <button className="bg-green-600 text-gray-100 p-2 mt-5 rounded-md shadow-xl">Enviar comprovante</button>
                 </div>
 
 
