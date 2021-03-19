424af650efe93ba40e329553a0fba185c3a9abba
Rodrigo Brito <spensmusic@gmail.com>
2021-03-19 17:21:56 -0300

    redux persist

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,17 @@ import '../styles/globals.css'
 //import Head from 'next/head'
 import { Provider } from 'react-redux'
 import store from '../store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
--- store/store.js
+++ store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import paymentReducer from '../features/payment/paymentDetails'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, paymentReducer);
 
 export default configureStore({
     reducer: {
-        payment: paymentReducer
+        payment: persistedReducer
     }
 })
