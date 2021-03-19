3ca1c76a4f8874c748d8ab9f29ad44960270a10d
Rodrigo Brito <spensmusic@gmail.com>
2021-03-18 21:03:04 -0300

    add buttons

--- src/components/SorteioCard.js
+++ src/components/SorteioCard.js
@@ -8,7 +8,7 @@ function SorteioCard({ rifa }) {
 
     const Loader = () => {
         return (
-            <div className="text-white w-20">
+            <div className="text-white text-center w-20">
                 Carregando...
                 <svg className=" animate-spin h-5 w-5 mr-3">
                     <circle cx="1" cy="1" r="4" stroke="white" stroke-width="5" fill="none" />
@@ -26,13 +26,21 @@ function SorteioCard({ rifa }) {
         <div className=" place-self-center m-4 rounded-md shadow-xl  text-white text-center">
 
             <Link href="/sorteios/[id]" as={`/sorteios/${rifa.id}`}>
-                <button className="p-2">
+                <button onClick={rifa.status === 'open' ? handleClick : null} className="">
                     <img className="" src={API_URL + rifa.img_sorteio_page.formats.small.url} />
+
                 </button>
+
             </Link>
+            <Link href="/sorteios/[id]" as={`/sorteios/${rifa.id}`}>
+                {rifa.status === 'open' ? <button onClick={handleClick} className="bg-green-500 text-white p-2 rounded-md">COMPRAR</button> : <button disabled={true} className="bg-red-500 text-white p-2 rounded-md">CONCLUÍDA</button>}
 
+            </Link>
+            {
+                gotClicked ? <Loader /> : null
+            }
 
-        </div>
+        </div >
     )
 }
 
