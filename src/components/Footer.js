1d0ec4d939fbb9051248a6301d0ab25adb9c1530
Rodrigo Brito <spensmusic@gmail.com>
2021-03-20 16:59:18 -0300

    enable terms on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 		<div className="relative bottom-0 w-full grid grid-cols-1 tablet:grid-cols-2 text-center bg-black p-8" >
 			<div className="text-white">
 				<button className="font-bold mx-4"><Link href="/">Início</Link></button>
-				{/*<button className="font-bold mx-4"><Link href="/termos">Termos</Link></button>*/}
+				<button className="font-bold mx-4"><Link href="/termos">Termos</Link></button>
 				<button className="font-bold mx-4"><Link href="/contato">Contato</Link></button>
 			</div>
 			<div className="text-white ">All rights reserved.</div>
